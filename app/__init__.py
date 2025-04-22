0baecf3d845a52a14b55ab20654fafb61f477487
Cam <23390625@student.uwa.edu.au>
2025-04-22 12:07:50 +0800

    Add basic server functions and HTML templates
    - populated __init__.py with basic server setup and routes
    - created base HTML template which includes the site navbar and base stylesheet
    - modified index.html to extend from base.html
    - modified file_upload.html to extend from base.html

---
+++ app/__init__.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, url_for
+from flask_login import LoginManager, current_user
+import logging
+
+def create_app(config_name="development"):
+    app = Flask(__name__, static_folder='static', template_folder='templates')
+
+    logging.basicConfig(level=logging.DEBUG)
+    app.logger.setLevel(logging.DEBUG)
+
+    # from .routes.user import user
+    # app.register_blueprint(user)
+
+    app.logger.debug(f"Registered blueprints: {app.blueprints}")
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    @app.route('/file_upload')
+    def file_upload():
+        return render_template('user/file_upload.html')
+    
+    @app.route('/login')
+    def login():
+        return render_template('auth/login.html')
+    
+    # login_manager = LoginManager()
+    # login_manager.init_app(app)
+    # login_manager.login_view = 'auth.login'
+
+    # @login_manager.user_loader
+    # def load_user(user_id):
+    #     from .models import User
+    #     return User.query.get(int(user_id))
+    
+    
+    return app
