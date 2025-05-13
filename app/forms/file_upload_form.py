b8b2302f376c043211e9bee4a62780bee110d737
Cam <23390625@student.uwa.edu.au>
2025-05-13 14:52:23 +0800

    Add broker field to file upload form
    - Add a field for choosing the crypto broker above current file upload field
    - Added validation message for new field to constants.py

--- app/forms/file_upload_form.py
+++ app/forms/file_upload_form.py
@@ -1,10 +1,14 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
-from wtforms import SubmitField
+from wtforms import SubmitField, RadioField
+from wtforms.validators import InputRequired
 # Above are the tools needed to use and validate flask forms
 import app.constants as msg
 
 class FileUploadForm(FlaskForm):
+    broker = RadioField('Crypto Broker:', choices=[('kraken', 'Kraken'), ('binance', 'Binance'), ('other', 'Other')], validators=[
+        InputRequired(message=msg.BROKER_REQUIRED)
+    ])
     file = FileField('Upload CSV', validators=[
         FileRequired(),
         FileAllowed(['csv'], msg.CSV_ONLY)
