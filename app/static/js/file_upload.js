30c19fda2435ce4b612e5f37faa1cfd66f118c81
Cam <23390625@student.uwa.edu.au>
2025-05-14 17:40:38 +0800

    Add scroll animation to file upload form
    - Added javascript to add a scrolling animation after the broker is selected.

--- app/static/js/file_upload.js
+++ app/static/js/file_upload.js
@@ -1,32 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const   btnArray    = document.querySelectorAll('.broker-img'), 
-            content0    = document.getElementsByClassName('form-first-part'),
-            content1    = document.getElementsByClassName('form-second-part'),
-            content2    = document.getElementsByClassName('form-third-part'),
-            backBtn     = document.getElementById('back-btn'),
-            formState   = document.getElementById('form-state').dataset.form_state;
+    const   content0    = document.getElementById('form-first-part'),
+            content1    = document.getElementById('form-second-part'),
+            content2    = document.getElementById('form-third-part'),
+            formState   = document.getElementById('form-state').dataset.form_state;;
 
     if (formState === '1' || formState === null) {
-        content0[0].style.display = "block";
-        content1[0].style.display = "none";
-        content2[0].style.display = "none";
+        content0.style.display = "block";
+        content1.style.display = "none";
+        content2.style.display = "none";
     }
     else if (formState === '2') {
-        content0[0].style.display = "none";
-        content1[0].style.display = "block";
-        content2[0].style.display = "block";
+        content0.style.display = "none";
+        content1.style.display = "block";
+        content2.style.display = "block";
     }
+    
+    document.querySelectorAll('.broker-img').forEach(function(img) {
+        img.addEventListener('click', function() {
+            // Scroll to the file upload section smoothl
+            content1.style.display = "block";
+            content2.style.display = "block";
 
-    btnArray.forEach(function(btn) {
-        btn.addEventListener('click', () => {
-            content0[0].style.display = "none";
-            content1[0].style.display = "block";
-            content2[0].style.display = "block";
+            content1.scrollIntoView({ behavior: 'smooth', alignToTop: false});
+            
+            setTimeout(() => {
+                content0.style.display = "none";
+            }, 500);
         });
     });
-    backBtn.addEventListener('click', () => {
-        content0[0].style.display = "block";
-        content1[0].style.display = "none";
-        content2[0].style.display = "none";
+
+    document.querySelector('#back-btn').addEventListener('click', function() {
+        // Scroll to the file upload section smoothly
+        content0.style.display = "block";
+
+        content0.scrollIntoView({ behavior: 'smooth', block: 'start'});
+
+        content1.style.display = "none";
+        content2.style.display = "none";
+        
     });
 });
